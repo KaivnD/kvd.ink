7069f7b5dcfeedb3d7bcd3c1d81cf9dde9999f83
KaivnD <KaivnD@hotmail.com>
2020-06-15 23:15:51 +0800

    opt:转移slick style

--- packages/website/pages/_app.tsx
+++ packages/website/pages/_app.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { Provider } from "react-redux";
 import { useStore, initializeStore } from "../common/store";
+import "../style.css";
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
@@ -26,17 +27,6 @@ export default function MyApp(props: AppProps) {
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
       </Head>
       <ThemeProvider theme={store.getState().theme}>
